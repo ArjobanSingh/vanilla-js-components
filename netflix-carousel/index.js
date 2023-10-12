b02b2a6eafef68f5fa0d75ff332413a4fc6b6b48
ArjobanSingh <arjobansingh1998@gmail.com>
2023-10-12 19:34:42 +0530

    add netflix series thumbnails

--- netflix-carousel/index.js
+++ netflix-carousel/index.js
@@ -1,35 +1,70 @@
 ((parent) => {
   const ITEMS_PER_PAGE = 4;
-  const NUM_OF_PAGES = 4;
+  const slider = parent.querySelector(".slider");
+
+  const IMAGES = [
+    "./images/vikings.webp",
+    "./images/vikings-valhalla.jpg",
+    "./images/extraction2.jpg",
+    "./images/fallout.webp",
+    "./images/peaky-blinders.jpg",
+    "./images/friday-night.jpg",
+    "./images/friends.webp",
+    "./images/ginny.jpg",
+    "./images/last-kingdom.jpg",
+    "./images/1899.jpg",
+    "./images/high-end.webp",
+    "./images/rrr.webp",
+    "./images/sex-education.jpg",
+    "./images/too-hot.jpg",
+    "./images/tu-jhoothi.webp",
+    "./images/witcher.jpg",
+  ];
+
+  const NUM_OF_PAGES = IMAGES.length / ITEMS_PER_PAGE;
+
   let isBeginning = true;
-  let slider = parent.querySelector(".slider");
+  let isTransitionInProgress = false;
+  let direction;
+
   const DIRECTION = {
     NEXT: "next",
     PREVIOUS: "previous",
   };
 
-  // although for perfect carousel, every page should be full
   let activePageIndex = 0;
   addListeners();
 
   function addListeners() {
     document.addEventListener("keydown", (e) => {
+      if (isTransitionInProgress) return;
       switch (e.key) {
         case "ArrowLeft":
           // like Netflix, on beginning we cannot go previous
           if (isBeginning) return;
           if (activePageIndex === 0) activePageIndex = NUM_OF_PAGES - 1;
           else activePageIndex--;
-          moveSlider(DIRECTION.PREVIOUS);
+          direction = DIRECTION.PREVIOUS;
+          moveSlider();
           break;
         case "ArrowRight":
           if (activePageIndex === NUM_OF_PAGES - 1) activePageIndex = 0;
           else activePageIndex++;
-          moveSlider(DIRECTION.NEXT);
+          direction = DIRECTION.NEXT;
+          moveSlider();
           break;
         default:
           break;
       }
+    });
+
+    slider.addEventListener("transitionstart", () => {
+      isTransitionInProgress = true;
+    });
+
+    slider.addEventListener("transitionend", () => {
+      addItemsAroundCurrentPage();
+      isTransitionInProgress = false;
     });
   }
 
@@ -37,17 +72,18 @@
     const fragment = document.createDocumentFragment();
     Array.from({ length: ITEMS_PER_PAGE }, (_, idx) => idx).forEach(
       (it, idx) => {
-        const itemIdx = pageNumber * ITEMS_PER_PAGE + idx + 1;
+        const itemIdx = pageNumber * ITEMS_PER_PAGE + idx;
+        const oneBasedIdx = itemIdx + 1;
+
         const sliderItem = document.createElement("li");
         sliderItem.classList.add("slider__item");
 
         const img = document.createElement("img");
-        img.src = "https://placehold.co/400x300";
-        img.alt = `Carousel item: ${itemIdx}`;
+        img.src = IMAGES[itemIdx];
+        img.alt = `Carousel item: ${oneBasedIdx}`;
 
         const indexIndicator = document.createElement("div");
         indexIndicator.classList.add("absolute__item");
-        indexIndicator.textContent = `Item ${itemIdx}`;
 
         sliderItem.append(img, indexIndicator);
         fragment.append(sliderItem);
@@ -63,13 +99,33 @@
       .forEach((element) => element.parentElement.removeChild(element));
   }
 
-  function getTranslateXValue(element) {
-    const style = getComputedStyle(element);
-    const matrix = new WebKitCSSMatrix(style.transform);
-    console.log("translateX: ", matrix.m41);
+  function addItemsAroundCurrentPage() {
+    const nextPageIdx =
+      activePageIndex === NUM_OF_PAGES - 1 ? 0 : activePageIndex + 1;
+    const previousPageIdx =
+      activePageIndex === 0 ? NUM_OF_PAGES - 1 : activePageIndex - 1;
+
+    const previousItems = createItemsForPage(previousPageIdx);
+    const nextItems = createItemsForPage(nextPageIdx);
+
+    // when user move forward, remove first 4, and append next 4
+    if (direction === DIRECTION.NEXT) {
+      if (!isBeginning) removeItemsBetweenIdx(0, ITEMS_PER_PAGE);
+      slider.append(nextItems);
+    }
+
+    // and vice-versa logic
+    if (direction === DIRECTION.PREVIOUS) {
+      removeItemsBetweenIdx(ITEMS_PER_PAGE * 2);
+      slider.prepend(previousItems);
+    }
+
+    if (isBeginning) isBeginning = false;
+    slider.style.transition = "none";
+    slider.style.transform = `translateX(-100%)`;
   }
 
-  function moveSlider(direction) {
+  function moveSlider() {
     slider.style.transition = "transform 0.3s ease-in-out";
     const translateVal = isBeginning
       ? -100
@@ -78,33 +134,10 @@
       : 0;
 
     slider.style.transform = `translateX(${translateVal}%)`;
-    getTranslateXValue(slider);
-
-    const nextPageIdx =
-      activePageIndex === NUM_OF_PAGES - 1 ? 0 : activePageIndex + 1; // 3
-    const previousPageIdx =
-      activePageIndex === 0 ? NUM_OF_PAGES - 1 : activePageIndex - 1; // 1
-
-    // TODO: manage with transitionEnd event instead of setTimeout
-    setTimeout(() => {
-      const previousItems = createItemsForPage(previousPageIdx);
-      const nextItems = createItemsForPage(nextPageIdx);
-
-      // when user move forward, remove first 4, and append next 4
-      if (direction === DIRECTION.NEXT) {
-        if (!isBeginning) removeItemsBetweenIdx(0, ITEMS_PER_PAGE);
-        slider.append(nextItems);
-      }
-
-      // and vice-versa logic
-      if (direction === DIRECTION.PREVIOUS) {
-        removeItemsBetweenIdx(ITEMS_PER_PAGE * 2);
-        slider.prepend(previousItems);
-      }
-
-      if (isBeginning) isBeginning = false;
-      slider.style.transition = "none";
-      slider.style.transform = `translateX(-100%)`;
-    }, 300);
   }
+
+  // create 2 pages initially
+  const firstPage = createItemsForPage(0);
+  const secondPage = createItemsForPage(1);
+  slider.append(firstPage, secondPage);
 })(document.body);
